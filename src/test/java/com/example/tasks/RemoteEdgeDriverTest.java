afc05d4d015d3b9cb809e8159ea9d69832b88bcd
maksim.gamov <nick141115@gmail.com>
2024-05-13 23:15:40 +0300

    Remote

---
+++ src/test/java/com/example/tasks/RemoteEdgeDriverTest.java
@@ -0,0 +1,36 @@
+package com.example.tasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RemoteEdgeDriverTest {
+    public static void main(String[] args) {
+        WebDriver driver;
+//        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        EdgeOptions edgeOptions = new EdgeOptions();
+
+        // Указываем адрес удаленного узла Selenium Grid
+        String gridUrl = "http://192.168.111.131:4444/wd/hub";
+
+        try {
+            // Инициализация драйвера для браузера Mozilla Firefox на удаленном узле
+//            driver = new RemoteWebDriver(new URL(gridUrl), firefoxOptions);
+            driver = new RemoteWebDriver(new URL(gridUrl), edgeOptions);
+
+            driver.manage().window().maximize();
+            driver.get("https://www.google.com");
+
+            // Дополнительные шаги теста
+            System.out.println(driver.getTitle());
+
+            // Закрытие браузера и завершение сеанса
+            driver.quit();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/example/tasks/RemoteTest.java
@@ -0,0 +1,26 @@
+package com.example.tasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class RemoteTest {
+    public static void main(String[] args) {
+        WebDriver driver;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome"); // Указываем браузер
+        capabilities.setPlatform(org.openqa.selenium.Platform.WINDOWS); // Указываем платформу
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.111.31:4444/wd/hub"), capabilities);
+            driver.manage().window().maximize();
+            driver.get("https://www.google.com");
+            System.out.println(driver.getTitle());
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
