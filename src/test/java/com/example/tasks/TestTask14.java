a5601ed506c4219152105dc4d3fd54dbbfd17b48
maksim.gamov <nick141115@gmail.com>
2024-06-04 16:49:17 +0300

    refactoringTestTask14

--- src/test/java/com/example/tasks/TestTask14.java
+++ src/test/java/com/example/tasks/TestTask14.java
@@ -1,5 +1,3 @@
-//        !!!Сделать цикл с параметром (for)!!!
-
 package com.example.tasks;
 
 import org.junit.Test;
@@ -15,8 +13,6 @@ public class TestTask14 extends BasePage.BasePage{
     @Test
     public void Task14(){
 
-//        !!!Сделать цикл с параметром (for)!!!
-
         get(URL_ADMIN);
         loginPass(driverChrome);
         clickLinkText("Countries");
@@ -25,114 +21,25 @@ public class TestTask14 extends BasePage.BasePage{
         // Найдем все элементы, соответствующие CSS селектору
         List<WebElement> elements = driverChrome.findElements(By.cssSelector("a[target='_blank']>i.fa-external-link"));
 
-        elements.get(0).click();
+        for (int i = 0; i < 7; i++)
+        {
+            elements.get(i).click();
 
-        // Получаем список всех вкладок
-        ArrayList<String> tabs = new ArrayList<>(driverChrome.getWindowHandles());
+            // Получаем список всех вкладок
+            ArrayList<String> tabs = new ArrayList<>(driverChrome.getWindowHandles());
 
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
+            // Закрываем вторую вкладку
+            driverChrome.switchTo().window(tabs.get(1));
 
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
+            if (i!=3)
+                {driverChrome.findElement(By.cssSelector("a.mw-logo"));}
+            else
+                {driverChrome.findElement(By.className("infa-h-logo"));}
 
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
+            driverChrome.close();
 
-        elements.get(1).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-
-        elements.get(2).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-
-        elements.get(3).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.className("infa-h-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-
-        elements.get(4).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-
-        elements.get(5).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-
-        elements.get(6).click();
-
-        // Получаем список всех вкладок
-        tabs = new ArrayList<>(driverChrome.getWindowHandles());
-
-        // Закрываем вторую вкладку
-        driverChrome.switchTo().window(tabs.get(1));
-        driverChrome.findElement(By.cssSelector("a.mw-logo"));
-        driverChrome.close();
-
-        // Переключаемся на первую вкладку
-        driverChrome.switchTo().window(tabs.get(0));
-
+            // Переключаемся на первую вкладку
+            driverChrome.switchTo().window(tabs.get(0));
+        }
     }
 }
